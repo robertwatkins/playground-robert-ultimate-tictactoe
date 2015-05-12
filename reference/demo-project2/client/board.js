92f00c15e15785d0f97facb8260df577a1e47898
robertwatkins <robert@watkins.net>
2015-05-12 11:51:07 -0500

    experimenting with drawing board in HTML5

--- reference/demo-project2/client/board.js
+++ reference/demo-project2/client/board.js
@@ -1,32 +1,39 @@
 function init() {
-	// draw a line from (100, 50) to (300, 175);
+	
 	var canvas = document.getElementById('myCanvas');
     var context = canvas.getContext('2d');
 
-    context.beginPath();
-    //context.moveTo(100, 150);
-    //context.lineTo(450, 50);
-    //context.stroke();
+	//drawing 'X'
 	context.beginPath();
-	context.strokeStyle('#555555');
-	context.lineWidth(15);
+	context.strokeStyle='#777777';
+	context.lineWidth=15;
 	context.moveTo(55, 55);
 	context.lineTo( 105, 105);
 	context.stroke();
 	context.moveTo(105,55);
-	context.line(55,105);
+	context.lineTo(55,105);
 	context.stroke();
 
-	//context.ellipse(200,80,50,50);
+	//drawing 'O'
+	context.beginPath();
+	context.strokeStyle='#777777';
+	context.lineWidth=15;
+	context.arc(200,80,27,0,2*Math.PI);
+	context.stroke();
 
-	// draw a thicker, green line from (350, 200) to (100, 310);
-	context.strokeStyle('#000000');
+	//drawing board
+	context.beginPath();
+	context.strokeStyle='#000000';
 	context.moveTo(140, 35);
-	context.line( 140, 360);
+	context.lineTo( 140, 360);
+	context.stroke();
 	context.moveTo(260, 35);
 	context.lineTo( 260, 360);
+	context.stroke();
 	context.moveTo(35,140);
 	context.lineTo(360,140);
+	context.stroke();
 	context.moveTo(35,245);
 	context.lineTo(360,245);
+	context.stroke();
 	}
